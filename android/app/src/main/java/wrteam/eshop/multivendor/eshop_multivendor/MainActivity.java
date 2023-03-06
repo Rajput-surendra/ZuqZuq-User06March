29b7beb16f13c8792a89ec4070d230f901613074
Rajput-surendra <surendraalphawizz@gmail.com>
2023-03-06 14:00:17 +0530

    first commit

---
+++ android/app/src/main/java/wrteam/eshop/multivendor/eshop_multivendor/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mytop10storeuser.user;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,19 @@
+final String appName = 'My Top 10 Store';
+
+final String packageName = 'com.mytop10storeuser.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.mytop10storeuser.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.mytop10storeuser.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+final String baseUrl = 'https://mytop10store.com/app/v1/api/';
+final String imageUrl = 'https://mytop10store.com/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
